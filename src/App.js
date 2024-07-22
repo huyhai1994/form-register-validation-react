4d85fafee38c743a41a4bac14b92db46fac9df45
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-22 21:17:26 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [form, setForm] = useState({});
+
+    /*TODO: Khởi tạo hàm handleChange nhận event làm param truyền vào, hàm này sẽ cập nhật lại giá trị mới cho form thông qua hàm setForm
+    */
+    function handleChange(event) {
+        setForm({
+            ...form, [event.target.name]: event.target.value
+        });
+    }
+
+    /*TODO: Khởi tạo hàm handleSubmit
+        Khai báo biến isValid mang giá trị bằng form.username && form.email && form.password && form.confirmPassword, biến này sẽ quyết định thông báo được đưa ra
+         Sử dụng hàm alert để đưa ra thông báo phù hợp
+            Nếu isValid bằng true thì thông báo đăng ký thành công
+        Nếu isValid bằng false thì thông báo người dùng điền đầy đủ thông tin*/
+    function handleSubmit() {
+        const isValid = form.username && form.email && form.password && form.confirmPassword;
+        alert(isValid ? "Sign in success!!!" : "Please fill out all the fields!!!");
+    }
+
+    /*TODO: Tạo hàm return, trả về là element JSX thể hiện các element của form Sign in
+    */
+    return (<div>
+        <h1>Sign up</h1>
+        <form>
+            <div className="custom-input">
+                <label>Username </label>
+                <input
+                    name="username"
+                    value={form.username || ""}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="custom-input">
+                <label>Email </label>
+                <input
+                    name="email"
+                    value={form.email || ""}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="custom-input">
+                <label>Password </label>
+                <input
+                    type="password"
+                    name="password"
+                    value={form.password || ""}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="custom-input">
+                <label>Confirm password </label>
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    value={form.confirmPassword || ""}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="button" onClick={handleSubmit}>
+                Submit
+            </button>
+        </form>
+    </div>);
 }
 
 export default App;
